3b89fa8be027b879c0f2fac7d4851eec214ed402
NoTaste <80740691+NoTaste-def@users.noreply.github.com>
2024-07-12 13:07:28 +0900

    Update 9-Palindrome-Number.py

--- LEETCODE/9-Palindrome-Number.py
+++ LEETCODE/9-Palindrome-Number.py
@@ -11,11 +11,11 @@ class Solution(object):
   def isPalindrome(self, x):
     if x < 0 :
         return False
-    def resursion(x, left, right):
+    def recursion(x, left, right):
         if left >= right:
             return True
         if x[left] != x[right]:
             return False
-        return resursion(x, left+1, right-1)
+        return recursion(x, left+1, right-1)
 
-    return resursion(str(x), 0, len(str(x))-1)
+    return recursion(str(x), 0, len(str(x))-1)
